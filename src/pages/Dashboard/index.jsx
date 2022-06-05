75ead36d90eecf1ce5f69c2c5649bc29e82dde2b
bcwin-i <iziqbek@gmail.com>
2022-06-05 23:09:23 +0000

    dashboard UI

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { MdSpaceDashboard, MdPayments, MdOutlineHistory } from "react-icons/md";
-import {IoMdLogOut} from "react-icons/io"
+import { IoMdLogOut } from "react-icons/io";
 import { useParams } from "react-router-dom";
 
 import { SidebarButtons } from "../../components";
@@ -40,16 +40,16 @@ const Dashboard = () => {
           />
         </div>
         <div style={{ marginTop: 50 }}>
-          <SidebarButtons title="Dashboard" active={(page === "Dashboard")}>
+          <SidebarButtons title="Dashboard" active={page === "Dashboard"}>
             <MdSpaceDashboard size={16} style={{ marginRight: 10 }} />
           </SidebarButtons>
-          <SidebarButtons title="Payment" active={(page === "Payment")}>
+          <SidebarButtons title="Payment" active={page === "Payment"}>
             <MdPayments size={16} style={{ marginRight: 10 }} />
           </SidebarButtons>
-          <SidebarButtons title="History" active={(page === "History")}>
+          <SidebarButtons title="History" active={page === "History"}>
             <MdOutlineHistory size={16} style={{ marginRight: 10 }} />
           </SidebarButtons>
-          <SidebarButtons title="Logout" >
+          <SidebarButtons title="Logout">
             <IoMdLogOut size={16} style={{ marginRight: 10 }} />
           </SidebarButtons>
         </div>
@@ -79,6 +79,130 @@ const Dashboard = () => {
             <FaUserCircle size={30} color={colors.secondary} />
           </div>
         </header>
+        <section style={{ flex: 1, padding: 20, flexDirection: "column" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <div
+              style={{
+                flex: 0.7,
+                borderRadius: 10,
+                backgroundColor: colors.secondary,
+                padding: 15,
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignSelf: "center",
+                }}
+              >
+                <text style={{ color: "white", fontSize: 40, fontWeight: 500 }}>
+                  Your TUITION
+                </text>
+                <p style={{ color: "white", fontSize: 18, fontWeight: 400 }}>
+                  payment in your own hand
+                </p>
+                <p
+                  style={{ color: colors.accent, fontSize: 14, marginTop: 30 }}
+                >
+                  A handy tool for all things payments. With benefits such a
+                  solution are multipurposeness, transparency and
+                  accountability.
+                </p>
+              </div>
+              <img
+                style={{ height: 250, width: 250, alignSelf: "flex-end" }}
+                src={require("../../assets/purse.png")}
+                alt="purse"
+              />
+            </div>
+            <div
+              style={{
+                flex: 0.28,
+                borderRadius: 10,
+                backgroundColor: colors.accent,
+                padding: 15,
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <text
+                style={{
+                  color: colors.primary,
+                  fontWeight: 400,
+                  fontSize: 20,
+                }}
+              >
+                Added cards
+              </text>
+              <span
+                style={{
+                  flexDirection: "row",
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                <img
+                  src={require("../../assets/visa.png")}
+                  style={{ height: 60, width: 60 }}
+                  alt="visa"
+                />
+                <h4
+                  style={{
+                    fontSize: 18,
+                    marginLeft: 20,
+                    display: "flex",
+                    alignSelf: "center",
+                  }}
+                >
+                  •••• •••• •••• 4456
+                </h4>
+              </span>
+              <span
+                style={{
+                  flexDirection: "row",
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                <img
+                  src={require("../../assets/visa.png")}
+                  style={{ height: 60, width: 60 }}
+                  alt="visa"
+                />
+                <h4
+                  style={{
+                    fontSize: 18,
+                    marginLeft: 20,
+                    display: "flex",
+                    alignSelf: "center",
+                  }}
+                >
+                  •••• •••• •••• 3454
+                </h4>
+              </span>
+            </div>
+          </div>
+            <div
+              style={{
+                borderRadius: 10,
+                backgroundColor: colors.accent,
+                padding: 15,
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: 20,
+                fontWeight: 600
+              }}
+            >Cash Balance <h3 style={{color: colors.secondary, fontSize: 30}}>GH₵ 5,653.33</h3></div>
+        </section>
       </div>
     </div>
   );
